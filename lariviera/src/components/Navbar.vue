6fd19dec8bed587e3b84e3abd12c0748d831f53e
Andres <andres.carvajal.garcia16@gmail.com>
2025-01-24 09:36:53 -0400

    Añadir pestañas de Galería y Eventos al Navbar

--- lariviera/src/components/Navbar.vue
+++ lariviera/src/components/Navbar.vue
@@ -48,20 +48,6 @@
           Ofertas
         </router-link>
         <router-link to="/about">
-          <!-- <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="size-6"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205 3 1m1.5.5-1.5-.5M6.75 7.364V3h-3v18m3-13.636 10.5-3.819"
-            />
-          </svg> -->
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -77,11 +63,45 @@
             />
           </svg>
 
-          Habitaciones
+          Paquetes
         </router-link>
         <router-link to="/about">
           <AcademicCapIcon class="w-6 h-6 justify-center" />
           <p class="">Nuestros Servicios</p>
+        </router-link>
+        <router-link to="/gallery">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M4.5 6.75h15M4.5 12h15m-15 5.25h15"
+            />
+          </svg>
+          <p class="">Galería</p>
+        </router-link>
+        <router-link to="/events">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M8.25 6.75h7.5M3 9.75h18m-18 6h18m-11.25 3v-3.75H6.75m10.5 0H12.75V19.5"
+            />
+          </svg>
+          <p class="">Eventos</p>
         </router-link>
       </div>
     </div>
