a3d07cc0ff8f396548885f82f573b74c74fb5f78
Andres <andres.carvajal.garcia16@gmail.com>
2025-01-29 01:47:21 -0400

    Implementacion de botones en tarjetas

---
+++ lariviera/src/views/BodaGoldView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <h1>Detalles del Paquete Goldsssssssssss: {{ paqueteId }}</h1>
+      <!-- Aquí puedes cargar la información del paquete usando el id -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: ['id'], // Recibe el id como prop
+    computed: {
+      paqueteId() {
+        return this.id;
+      }
+    },
+    // Aquí podrías hacer una llamada a una API o cargar los datos de la imagen, precio, etc.
+  };
+  </script>
+  
---
+++ lariviera/src/views/QuinceGoldView.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <h1>Quince Platinum</h1>
+      <p>Detalles sobre el paquete Quince Platinum.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'QuincePlatinumView'
+  };
+  </script>
+  
+  <style scoped>
+  /* Estilos específicos para esta vista */
+  </style>
+  
