5a6025dc283ac891dacd7d148dbf5ef0f79581e0
GuiladVillarroel <guilad1233@gmail.com>
2025-01-29 10:10:27 -0400

    add: HomeView nav fot

--- lariviera/src/components/Navbar.vue
+++ lariviera/src/components/Navbar.vue
@@ -1,110 +1,27 @@
 <template>
-  <nav class="bg-blue-950 font-serif">
-    <div class="flex px-4 justify-between">
-      <div class="flex">
-        <img
-          src="https://scontent.flpb3-1.fna.fbcdn.net/v/t39.30808-6/305998895_602548724646222_6734776312684364712_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=umH4yXIZ3CEQ7kNvgFyEkN-&_nc_zt=23&_nc_ht=scontent.flpb3-1.fna&_nc_gid=Au6uCaLZHFTzDVVHAqdrnZm&oh=00_AYA3rEH5GY--Lgsu3w0h2eoTMFcCHU9dxf_eaTaLZJa08w&oe=67944B17"
-          class="h-13 rounded-full w-14 m-2"
-        />
-        <p class="mt-5 mx-4 text-white font-serif text-3xl">LA RIVIERA</p>
-      </div>
-      <button class="lg:hidden">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="size-6 text-white-500"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-          />
-        </svg>
-      </button>
-      <div class="flex px-5 flex-row gap-5 mt-5 text-white mx-4 text-2xl">
-        <router-link to="/" class="">
-          <HomeIcon class="w-6 h-6" />
-          <p class="">Home</p>
-        </router-link>
-        <router-link to="/about">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="size-6"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="m9 14.25 6-6m4.5-3.493V21.75l-3.75-1.5-3.75 1.5-3.75-1.5-3.75 1.5V4.757c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0c1.1.128 1.907 1.077 1.907 2.185ZM9.75 9h.008v.008H9.75V9Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm4.125 4.5h.008v.008h-.008V13.5Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
-            />
-          </svg>
-
-          Ofertas
-        </router-link>
-        <router-link to="/about">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="size-6"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Z"
-            />
-          </svg>
-
-          Paquetes
-        </router-link>
-        <router-link to="/about">
-          <AcademicCapIcon class="w-6 h-6 justify-center" />
-          <p class="">Nuestros Servicios</p>
-        </router-link>
-        <router-link to="/gallery">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="size-6"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
-            />
-          </svg>
-
-          <p class="">Galería</p>
-        </router-link>
-        <router-link to="/events">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            class="size-6"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M6.75 2.25A.75.75 0 0 1 7.5 3v1.5h9V3A.75.75 0 0 1 18 3v1.5h.75a3 3 0 0 1 3 3v11.25a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V7.5a3 3 0 0 1 3-3H6V3a.75.75 0 0 1 .75-.75Zm13.5 9a1.5 1.5 0 0 0-1.5-1.5H5.25a1.5 1.5 0 0 0-1.5 1.5v7.5a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5v-7.5Z"
-              clip-rule="evenodd"
-            />
-          </svg>
-
-          <p class="">Eventos</p>
-        </router-link>
-      </div>
+  <nav class="bg-white-900 text-white p-4 flex justify-between items-center">
+    <div class="flex items-center">
+      <img src="@/assets/img/EPgold.jpeg" alt="Logo" class="h-10 mr-2" />
+      <router-link to="/" class="text-lg font-bold text-black"
+        >Salon de Eventos La Riviera</router-link
+      >
     </div>
+    <ul class="hidden md:flex space-x-6 text-black">
+      <li><router-link to="/" class="hover:text-sky-700">Inicio</router-link></li>
+      <li>
+        <router-link to="/Ofertas" class="hover:text-sky-700">Eventos</router-link>
+      </li>
+
+      <li>
+        <router-link to="/Servicios" class="hover:text-sky-700">Servicios</router-link>
+      </li>
+      <li>
+        <router-link to="/paquetes" class="hover:text-sky-700">Paquetes</router-link>
+      </li>
+    </ul>
+    <button class="bg-sky-700 hover:bg-gray-700 px-4 py-2 rounded text-white">
+      Reservar
+    </button>
   </nav>
 </template>
 
--- lariviera/src/views/HomeView.vue
+++ lariviera/src/views/HomeView.vue
@@ -3,13 +3,41 @@ import ChatBot from "@/components/ChatBot.vue";
 </script>
 
 <template>
-  <div class="">
-    <img
-      src="https://www.boliviaentusmanos.com/amarillas1/businesscard/imagenes/la-riviera-centro-de-eventos-y-convenciones-3.jpg"
-      alt=""
-      class="w-full"
-    />
-
+  <div class="relative w-full h-screen">
+    <div class="absolute inset-0">
+      <img src="@/assets/img/BAUTIZO.jpeg" alt="Hotel" class="w-full h-full opacity-80" />
+    </div>
+    <div class="absolute left-0 right-0 p-8">
+      <p class="text-3xl font-bold mb-8 justify-center flex text-gray">
+        Nuestros Servicios
+      </p>
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <div class="bg-white p-6 rounded-lg shadow-lg">
+          <img src="@/assets/img/EpEstandar.jpeg" alt="" class="h-48 w-full" />
+          <h3 class="text-xl font-bold">15 años</h3>
+          <p class="text-gray-600">190 Bs por persona</p>
+          <p>
+            El paquete Gold incluye: Decoracion personalizada, catering gourmet y DJ
+            profesional
+          </p>
+        </div>
+        <div class="bg-white p-6 rounded-lg shadow-lg">
+          <img src="@/assets/img/EPGold.jpeg" alt="" class="h-48 w-full" />
+          <h3 class="text-xl font-bold">Matrimonios</h3>
+          <p class="text-gray-600">
+            Disfruta de una deliciosa gastronomía en nuestro restaurante.
+          </p>
+        </div>
+        <div class="bg-white p-6 rounded-lg shadow-lg">
+          <img src="@/assets/img/EPPlatinum.jpeg" alt="" class="h-48 w-full" />
+          <h3 class="text-xl font-bold">Varios</h3>
+          <p class="text-gray-600">
+            Relájate y renueva energías en nuestro exclusivo spa.
+          </p>
+        </div>
+      </div>
+    </div>
     <ChatBot />
   </div>
 </template>
+<style></style>
