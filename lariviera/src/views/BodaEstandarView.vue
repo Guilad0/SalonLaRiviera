48b25aca8af78c902b4269c5e4d990c1a6834438
Andres <andres.carvajal.garcia16@gmail.com>
2025-02-28 10:24:50 -0400

    Actualización y responsividad

--- lariviera/src/views/BodaEstandarView.vue
+++ lariviera/src/views/BodaEstandarView.vue
@@ -1,137 +1,146 @@
 <template>
-   <div class="m-5 p-2.5 font-serif text-2xl" style="font-family: 'Comfortaa', sans-serif;">
-    <h1 class="text-1xl font-[Mode G]">Detalles del Paquete Estándar: {{ paqueteId }}</h1>
+  <div class="overflow-x-auto w-full">
+    <div class="m-5 p-2.5 font-serif text-2xl" style="font-family: 'Comfortaa', sans-serif;">
+      <h1 class="text-1xl font-[Mode G] text-center">Detalles del Paquete Estándar: {{ paqueteId }}</h1>
 
-    <h2 class="text-justify italic carta">
-      "Estimados novios, Es un placer para nosotros ser parte de este momento tan especial
-      en sus vidas. En nombre del <b>Salón de Eventos La Riviera</b>, nos especializamos
-      en hacer de cada boda un evento único e inolvidable, cuidando cada detalle para que
-      disfruten de una celebración mágica y sin preocupaciones. A continuación, les
-      presentamos nuestra propuesta de cotización, diseñada para adaptarse a sus
-      necesidades y hacer realidad la boda de sus sueños. Quedamos atentos a cualquier
-      consulta o ajuste que deseen realizar. Será un honor acompañarlos en este gran día."
-    </h2>
+      <h2 class="text-justify italic carta px-4">
+        "Estimados novios, Es un placer para nosotros ser parte de este momento tan especial
+        en sus vidas. En nombre del <b>Salón de Eventos La Riviera</b>, nos especializamos
+        en hacer de cada boda un evento único e inolvidable, cuidando cada detalle para que
+        disfruten de una celebración mágica y sin preocupaciones. A continuación, les
+        presentamos nuestra propuesta de cotización, diseñada para adaptarse a sus
+        necesidades y hacer realidad la boda de sus sueños. Quedamos atentos a cualquier
+        consulta o ajuste que deseen realizar. Será un honor acompañarlos en este gran día."
+      </h2>
 
-    <h3 class="underline">
-      A continuación, detallamos los servicios y opciones incluidas en nuestra cotización,
-      diseñados para cubrir todas las necesidades de su boda y asegurar que sea un evento
-      memorable.
-    </h3>
+      <h3 class="underline text-center px-4">
+        A continuación, detallamos los servicios y opciones incluidas en nuestra cotización,
+        diseñados para cubrir todas las necesidades de su boda y asegurar que sea un evento
+        memorable.
+      </h3>
 
-    <div>
-      <table class="table-auto w-full border-collapse mt-4">
-        <tbody >
-          <!-- Fila 1 -->
-          <tr>
-            <td class="border px-4 py-2 text-left align-top">
-              <p class="text-justify italic font-bold">Brindis</p>
-              <p class="text-justify italic">• Cerezer</p>
-            </td>
-            <td class="border px-4 py-2 text-left align-top">
-              <p class="text-justify italic font-bold">Saladitos</p>
-              <p class="text-justify italic">• Nachos c/palta</p>
-            </td>
-            <td class="border px-4 py-2 text-left align-top">
-              <p class="text-justify italic font-bold">Montaje</p>
-              <p class="text-justify italic">• Cena un tipo de carne a elección</p>
-              <p class="text-justify italic">• Vajilla, cristalería.</p>
-              <p class="text-justify italic">• Cubertería.</p>
-            </td>
-          </tr>
-          <!-- Fila 2 -->
-          <tr>
-            <td class="border px-4 py-2 text-left align-top">
-              <p class="text-justify italic font-bold">Bebidas en mesa con alcohol (2)</p>
-              <p class="text-justify italic">• Singani Casa Real</p>
-              <p class="text-justify italic">• Ron Carta Vieja - Abuelo</p>
-              <p class="text-justify italic">• Ron Carta Vieja- Havana</p>
-              <p class="text-justify italic">• Ron Solera</p>
-            </td>
-            <td class="border px-4 py-2 text-left align-top">
-              <p class="text-justify italic font-bold">
-                Bebidas sin alcohol (canilla libre)
-              </p>
-              <p class="text-justify italic">• Gaseosas /hielo/limón/agua</p>
-            </td>
-            <td class="border px-4 py-2 text-left align-top">
-              <p class="text-justify italic font-bold">Fondeu de chocolate</p>
-              <p class="text-justify italic">• Frutas de temporada</p>
-              <p class="text-justify italic">• Mashmelos</p>
-            </td>
-          </tr>
-          <!-- Fila 3 -->
-          <tr>
-            <td class="border px-4 py-2 text-left align-top">
-              <p class="text-justify italic font-bold">Montaje</p>
-              <p class="text-justify italic">• Mesas, sillas tifany</p>
-              <p class="text-justify italic">• Vajilla, cristalería.</p>
-              <p class="text-justify italic">• Cubertería.</p>
-            </td>
-            <td class="border px-4 py-2 text-left align-top">
-              <p class="text-justify italic font-bold">Personal de atención</p>
-              <p class="text-justify italic">• Coordinador de Eventos.</p>
-              <p class="text-justify italic">• Garzones</p>
-              <p class="text-justify italic">• Personal de seguridad</p>
-              <p class="text-justify italic">
-                • Personal de limpieza durante todo el evento
-              </p>
-            </td>
-            <td class="border px-4 py-2 text-left align-top">
-              <p class="text-justify italic font-bold">Propuesta de servicio</p>
-              <p class="text-justify italic">• Cronograma de actividades</p>
-              <p class="text-justify italic">• Ambiente para descanso novios.</p>
-            </td>
-          </tr>
-          <!-- Fila larga (una sola columna que cubre tres columnas) -->
-          <tr>
-            <td colspan="3" class="border px-4 py-2 text-left align-top">
-              <p class="text-justify italic font-bold">Detalles del plato servido</p>
-              <p class="text-justify italic font-bold">Carnes:</p>
-              <p class="text-justify italic">
-                • Peceto relleno/medallón de pollo /filete de cerdo al vino
-              </p>
-              <p class="text-justify italic font-bold">Guarniciones:</p>
-              <p class="text-justify italic">
-                • Papa salteada al vino/croqueta de yuca/pastel de papa/arroz chifa
-              </p>
-              <p class="text-justify italic font-bold">Ensaladas</p>
-              <p class="text-justify italic">
-                • Salsa waldorf/salsa alemana con millo/salsa griega/ verduras al vapor
-              </p>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <div class="overflow-x-auto">
+        <table class="table-auto w-full border-collapse mt-4">
+          <tbody>
+            <tr>
+              <td class="border px-4 py-2 text-left align-top">
+                <p class="text-justify italic font-bold">Brindis</p>
+                <p class="text-justify italic">• Cerezer</p>
+              </td>
+              <td class="border px-4 py-2 text-left align-top">
+                <p class="text-justify italic font-bold">Saladitos</p>
+                <p class="text-justify italic">• Nachos c/palta</p>
+              </td>
+              <td class="border px-4 py-2 text-left align-top">
+                <p class="text-justify italic font-bold">Plato Sevido</p>
+                <p class="text-justify italic">• Cena un tipo de carne a elección</p>
+                <p class="text-justify italic">• Vajilla, cristalería.</p>
+                <p class="text-justify italic">• Cubertería.</p>
+              </td>
+            </tr>
+            <!-- Fila 2 -->
+            <tr>
+              <td class="border px-4 py-2 text-left align-top">
+                <p class="text-justify italic font-bold">Bebidas en mesa con alcohol (2)</p>
+                <p class="text-justify italic">• Singani Casa Real</p>
+                <p class="text-justify italic">• Ron Carta Vieja - Abuelo</p>
+                <p class="text-justify italic">• Ron Carta Vieja- Havana</p>
+                <p class="text-justify italic">• Ron Solera</p>
+
+              </td>
+              <td class="border px-4 py-2 text-left align-top">
+                <p class="text-justify italic font-bold">Bebidas sin alcohol (canilla libre)</p>
+                <p class="text-justify italic">• Gaseosas /hielo/limón/agua</p>
+              </td>
+              <td class="border px-4 py-2 text-left align-top">
+                <p class="text-justify italic font-bold">Fondeu de chocolate</p>
+                <p class="text-justify italic">• Frutas de temporada</p>
+                <p class="text-justify italic">• Mashmelos</p>                
+              </td>
+            </tr>
+             <!-- Fila 3 -->
+             <tr>
+              <td class="border px-4 py-2 text-left align-top">
+                <p class="text-justify italic font-bold">Montaje</p>
+                <p class="text-justify italic">• Mesas, sillas tifany</p>
+                <p class="text-justify italic">• Vajilla, cristalería</p>
+                <p class="text-justify italic">• Cubertería</p>
+              </td>
+              <td class="border px-4 py-2 text-left align-top">
+                <p class="text-justify italic font-bold">Saladitos</p>
+                <p class="text-justify italic">• Vajilla, cristalería.</p>
+                <p class="text-justify italic">• Cubertería.</p>
+              </td>
+              <td class="border px-4 py-2 text-left align-top">
+                <p class="text-justify italic font-bold">Personal de atención</p>
+                <p class="text-justify italic">• Coordinador de Eventos</p>
+                <p class="text-justify italic">• Garzones</p>
+                <p class="text-justify italic">• Personal de seguridad</p>
+                <p class="text-justify italic">• Personal de limpieza durante todo el evento</p>
+              </td>
+            </tr>
+             <!-- Fila sola -->
+             <tr>
+              <td colspan="3" class="border px-4 py-2 text-left align-top">
+                <p class="text-justify italic font-bold">Detalles del plato servido</p>              
+              
+                <p class="text-justify italic font-bold">Saladitos</p>
+                <p class="text-justify italic">Carnes:</p>
+                <p class="text-justify italic">• Peceto relleno/medallón de pollo /filete de cerdo al vino</p>           
+                <p class="text-justify italic font-bold">Guarniciones:</p>
+                <p class="text-justify italic">• Papa salteada al vino/croqueta de yuca/pastel de papa/arroz chifa</p>
+                <p class="text-justify italic">Ensaladas</p>
+                <p class="text-justify italic">• Salsa waldorf/salsa alemana con millo/salsa griega/ verduras al vapor</p>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <br /><br />
+      <h3 class="text-justify italic font-bold text-center px-4">
+        Calcula el Costo Total según el Número de Invitados, previo reunión con el coordinador
+      </h3>
+      <br />
+
+
+      <div class="w-full max-w-2xl mx-auto px-4">
+        <!-- Tabla Responsiva -->
+        <div class="overflow-x-auto">
+          <table class="w-full border-collapse mt-4">
+            <thead>
+              <tr class="bg-gray-100">
+                <th class="px-4 py-2 text-left text-sm font-medium text-gray-700">Detalle</th>
+                <th class="px-4 py-2 text-left text-sm font-medium text-gray-700">Valor</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr class="border-b">
+                <td class="px-4 py-2 text-sm text-gray-600">Cantidad de personas</td>
+                <td class="px-4 py-2 text-sm text-black font-bold text-600 recuadro">
+                  <input type="number" v-model="cantidad" min="0"
+                    class="w-full border rounded-md px-2 py-1 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500" />
+                </td>
+              </tr>
+              <tr class="border-b">
+                <td class="px-4 py-2 text-sm text-gray-600">Precio Unitario</td>
+                <td class="px-4 py-2 text-sm">{{ precioUnitario }} Bs</td>
+              </tr>
+              <tr>
+                <td class="px-4 py-2 text-sm text-gray-600">Total</td>
+                <td class="px-4 py-2 text-sm font-semibold">{{ total }} Bs</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+
+
+
+
     </div>
-    <br /><br />
-    <h3 class="text-justify italic font-bold">
-      Calcula el Costo Total según el Número de Invitados, previo reunión con el
-      coordinador
-    </h3>
-    <br />
-
-    <!-- Tabla de cotización -->
-    <table class="table-auto w-full border-collapse mt-4">
-      <thead>
-        <tr>
-          <th class="px-4 py-2 border">Descripción</th>
-          <th class="px-4 py-2 border">Cantidad de Personas</th>
-          <th class="px-4 py-2 border">Precio Unitario</th>
-          <th class="px-4 py-2 border">Total</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td class="border px-4 py-2">Paquete Estándar</td>
-          <td class="border px-4 py-2">
-            <input type="number" v-model="cantidad" min="0" class="cantidad-personas" />
-          </td>
-          <td class="border px-4 py-2">{{ precioUnitario }} Bs</td>
-          <td class="border px-4 py-2">{{ total }} Bs</td>
-        </tr>
-      </tbody>
-    </table>
   </div>
+
 </template>
 
 <script>
@@ -139,8 +148,8 @@ export default {
   props: ["id"],
   data() {
     return {
-      cantidad: 0, // Comienza con 0
-      precioUnitario: 185, // Precio de ejemplo
+      cantidad: 0,
+      precioUnitario: 185,
     };
   },
   computed: {
@@ -157,11 +166,9 @@ export default {
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700&family=Lora:wght@400;700&family=Great+Vibes&display=swap');
 
-
-/* El resto de los estilos */
-table {
-  width: 100%;
-  border-collapse: collapse;
+.table-auto {
+  max-width: 100%;
+  overflow-x: auto;
 }
 
 th,
@@ -172,50 +179,14 @@ td {
 }
 
 th {
-  background-color: #f2f2f2;
+  background-color: #1b1e24;
+  color: #D5DDE5;
+  font-size: 20px;
+  font-weight: 500;
 }
 
 tr:nth-child(even) {
   background-color: #f9f9f9;
-}
-
-.input-cantidad {
-  width: 60px;
-  text-align: center;
-  border: 2px solid #ccc;
-  padding: 5px;
-  font-size: 16px;
-  background-color: white;
-  box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.1);
-}
-
-/* Estilos para la tabla */
-.table-auto {
-  background: white;
-  border-radius: 3px;
-  border-collapse: collapse;
-  margin: auto;
-  max-width: 800px;
-  width: 100%;
-  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.1);
-  animation: float 5s infinite;
-}
-
-th {
-  background: #1b1e24;
-  color: #D5DDE5;
-  font-size: 20px;
-  font-weight: 500;
-  padding: 16px;
-  text-align: left;
-  text-transform: uppercase;
-}
-
-tr {
-  border-top: 1px solid #C1C3D1;
-  border-bottom: 1px solid #C1C3D1;
-  color: #666B85;
-  font-size: 16px;
 }
 
 tr:hover td {
@@ -223,77 +194,93 @@ tr:hover td {
   color: #FFFFFF;
 }
 
-tr:nth-child(odd) td {
-  background: #EBEBEB;
-}
-
-tr:nth-child(odd):hover td {
-  background: #4E5066;
-}
-
-td {
-  background: #FFFFFF;
-  padding: 16px;
-  text-align: left;
-  font-weight: 300;
-  font-size: 16px;
-  border-right: 1px solid #C1C3D1;
-}
-
-td:last-child {
-  border-right: 0px;
-}
-
-td[colspan="3"] {
-  background: #f9f9f9;
-  font-weight: bold;
-}
-
 .cantidad-personas {
-  color: red;
-  font-weight: bold;
+  width: 100%;
+  text-align: center;
   border: 2px solid red;
-  padding: 20px;
+  padding: 5px;
 }
 
-h1 {
-  font-family: 'Playfair Display', serif;
-  font-weight: 700;
-  color: #333;
-  text-align: center;
-  margin-bottom: 20px;
-  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
-  letter-spacing: 2px;
-  font-size: 3rem;
+
+@media (max-width: 480px) {
+
+  .responsive-th,
+  .responsive-td {
+    font-size: 10px;
+    padding: 4px;
+  }
+
+  .responsive-input {
+    width: 40px;
+    /* Ajuste adicional para pantallas muy pequeñas */
+    font-size: 9px;
+    padding: 2px;
+  }
 }
 
-h2 {
-  font-family: 'Playfair Display', serif;
-  font-weight: 700;
-  color: #333;
-  text-align: center;
-  margin-bottom: 20px;
-  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
-  letter-spacing: 2px;
-}
 
+h1,
+h2,
 h3 {
   font-family: 'Playfair Display', serif;
   font-weight: 700;
   color: #333;
-  text-align: center;
-  margin-bottom: 20px;
   text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
   letter-spacing: 2px;
-  font-size: 1.5rem;
 }
 
-p, td {
-  font-family: 'Lora', serif;
+h1 {
+  font-size: 2rem;
 }
 
 .carta {
   font-family: 'Great Vibes', cursive;
   font-size: 2rem;
+  text-align: center;
+}
+
+@media (max-width: 768px) {
+  .table-auto {
+    display: block;
+    overflow-x: auto;
+    white-space: nowrap;
+  }
+
+  th,
+  td {
+    font-size: 14px;
+    padding: 8px;
+  }
+
+  h1 {
+    font-size: 1.5rem;
+  }
+
+  .carta {
+    font-size: 1.2rem;
+  }
+
+  .table-auto tbody tr {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .table-auto tbody td {
+    width: 100%;
+    text-align: left;
+  }
+
+  .table-auto tbody td:before {
+    content: attr(data-label);
+    float: left;
+    font-weight: bold;
+    text-transform: uppercase;
+  }
+}
+
+.recuadro {
+
+  color: red;
+  border: red 2px solid;
 }
 </style>
--- lariviera/src/views/OtrosView.vue
+++ lariviera/src/views/OtrosView.vue
@@ -1,79 +1,82 @@
 <template>
-  <div class="m-5 p-2.5 font-serif text-2xl" style="font-family: 'Comfortaa', sans-serif;">
-    <h1 class="text-1xl font-[Mode G]">Detalles del Evento de Empresa: {{ paqueteId }}</h1>
-    <h2 class="text-justify italic">
+  <div class="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-6 font-serif">
+    <!-- Título -->
+    <h1 class="text-xl sm:text-2xl lg:text-3xl font-bold mb-4" style="font-family: 'Mode G', sans-serif;">
+      Detalles del Evento de Empresa: {{ paqueteId }}
+    </h1>
+
+    <!-- Introducción -->
+    <div class="text-sm sm:text-base text-justify italic mb-6 space-y-4">
       <p><b>"Estimados organizadores y equipo,</b></p>
+      <p>
+        Es un honor para nosotros ser parte de su próximo evento. En nombre del
+        <b>Salón de Eventos La Riviera</b>, nos especializamos en crear experiencias
+        memorables, brindando un servicio impecable y cuidando cada detalle para garantizar
+        el éxito de su reunión. A continuation, les presentamos nuestra propuesta de
+        cotización, diseñada para adaptarse a sus necesidades y hacer de su evento
+        corporativo, seminario o disertación una experiencia profesional y enriquecedora.
+        Quedamos atentos a cualquier consulta o ajuste que deseen realizar. Será un placer
+        acompañarlos en la organización de este importante encuentro."
+      </p>
+    </div>
 
-      Es un honor para nosotros ser parte de su próximo evento. En nombre del
-      <b>Salón de Eventos La Riviera</b>, nos especializamos en crear experiencias
-      memorables, brindando un servicio impecable y cuidando cada detalle para garantizar
-      el éxito de su reunión. A continuación, les presentamos nuestra propuesta de
-      cotización, diseñada para adaptarse a sus necesidades y hacer de su evento
-      corporativo, seminario o disertación una experiencia profesional y enriquecedora.
-      Quedamos atentos a cualquier consulta o ajuste que deseen realizar. Será un placer
-      acompañarlos en la organización de este importante encuentro."
-    </h2>
-
-    <h3 class="underline">
+    <!-- Subtítulo -->
+    <h3 class="text-base sm:text-lg underline mb-6">
       A continuación, detallamos los servicios y opciones incluidas en nuestra cotización,
       diseñados para cubrir todas las necesidades de su evento empresarial, seminario o
       disertación, garantizando una experiencia profesional y exitosa.
     </h3>
-    `
-    <div>
-      <table class="table-auto w-full border-collapse mt-4">
-        <!-- <thead>
-    <tr>
-      <th class="border px-4 py-2">Columna 1</th>
-      <th class="border px-4 py-2">Columna 2</th>
-      <th class="border px-4 py-2">Columna 3</th>
-    </tr>
-  </thead> -->
+
+    <!-- Tabla de servicios -->
+    <div class="overflow-x-auto">
+      <table class="w-full border-collapse mt-4 text-sm sm:text-base">
         <tbody>
           <!-- Fila 1 -->
-          <tr>
-            <td class="border px-4 py-2 text-left align-top">
-              <p class="text-justify italic font-bold">REQURIMENTO DE SALÓN</p>
-              <p class="text-justify italic">♦ SALÓN AMPLIO AIRE ACONDICIONADO</p>
-              <p class="text-justify italic">♦ 500 SILLAS TIFFANY</p>
-              <p class="text-justify italic">♦ MESAS DE APOYO</p>
-              <p class="text-justify italic">♦ ACCESO A BAÑOS EQUIPADOS</p>
-              <p class="text-justify italic">♦ AREAS DE JARDIN PARA DESCANSO</p>
+          <tr class="border-b">
+            <td class="px-4 py-3 text-left align-top">
+              <p class="text-justify italic font-bold">REQUERIMIENTO DE SALÓN</p>
+              <ul class="list-none space-y-1 mt-1">
+                <li class="text-justify italic">♦ SALÓN AMPLIO AIRE ACONDICIONADO</li>
+                <li class="text-justify italic">♦ 200 SILLAS TIFFANY</li>
+                <li class="text-justify italic">♦ MESAS DE APOYO</li>
+                <li class="text-justify italic">♦ ACCESO A BAÑOS EQUIPADOS</li>
+                <li class="text-justify italic">♦ ÁREAS DE JARDÍN PARA DESCANSO</li>
+              </ul>
             </td>
-
-            <td class="border px-4 py-2 text-left align-top">
-              <p class="text-justify italic font-bold">Bs 6000</p>
+            <td class="px-4 py-3 text-left align-top font-bold italic w-28 sm:w-32">
+              Bs 6000
             </td>
           </tr>
           <!-- Fila 2 -->
-          <tr>
-            <td class="border px-4 py-2 text-left align-top">
+          <tr class="border-b">
+            <td class="px-4 py-3 text-left align-top">
               <p class="text-justify italic font-bold">HERRAMIENTAS AUDIOVISUALES</p>
-              <p class="text-justify italic">♦ PANTALLA LED</p>
-              <p class="text-justify italic">♦ SONIDO DOS MICRÓFONOS</p>
-              <p class="text-justify italic">♦ SONIDO DOS MICRÓFONOS</p>
+              <ul class="list-none space-y-1 mt-1">
+                <li class="text-justify italic">♦ PANTALLA LED</li>
+                <li class="text-justify italic">♦ SONIDO DOS MICRÓFONOS</li>
+              </ul>
             </td>
-
-            <td class="border px-4 py-2 text-left align-top">
-              <p class="text-justify italic font-bold">Bs 1800</p>
+            <td class="px-4 py-3 text-left align-top font-bold italic w-28 sm:w-32">
+              Bs 1800
             </td>
           </tr>
-          <!-- Fila 3 -->
-
-
         </tbody>
       </table>
-      <br><br>
-      <h2 class="text-2xl font-bold italic">ADICIONAL:</h2>
-      <ul class="list-disc list-inside space-y-2 mt-2">
+    </div>
+
+    <!-- Sección Adicional -->
+    <div class="mt-8">
+      <h2 class="text-xl sm:text-2xl font-bold italic mb-4">ADICIONAL:</h2>
+      <ul class="list-disc list-inside space-y-2 text-sm sm:text-base">
         <li class="text-justify">
           Servicio de catering para todo tipo de eventos Censa Gourmet: banquetes,
           servicios de brunch, mesas dulces, mesas saladas, etc. (según requerimiento).
         </li>
       </ul>
 
-      <h2 class="text-2xl font-bold italic mt-4">EXTRAS:</h2>
-      <ul class="list-disc list-inside space-y-2 mt-2">
+      <!-- Sección Extras -->
+      <h2 class="text-xl sm:text-2xl font-bold italic mt-6 mb-4">EXTRAS:</h2>
+      <ul class="list-disc list-inside space-y-2 text-sm sm:text-base">
         <li class="text-justify">
           Amplificación y sonido ambiental, ajustados según los requerimientos del evento.
         </li>
@@ -85,34 +88,6 @@
         </li>
       </ul>
     </div>
-    <br /><br />
-    <!-- <h3 class="text-justify italic font-bold">
-      Calcula el Costo Total según el Número de Invitados, previo reunion con el
-      coordinador
-    </h3> -->
-    <br />
-
-    <!-- Tabla de cotización -->
-    <!-- <table>
-      <thead>
-        <tr>
-          <th>Descripción</th>
-          <th>Cantidad de Personas</th>
-          <th>Precio Unitario</th>
-          <th>Total</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Eventos de Empresa</td>
-          <td>
-            <input type="number" v-model="cantidad" min="0" class="input-cantidad" />
-          </td>
-          <td>{{ precioUnitario }} Bs</td>
-          <td>{{ total }} Bs</td>
-        </tr>
-      </tbody>
-    </table> -->
   </div>
 </template>
 
@@ -121,8 +96,8 @@ export default {
   props: ["id"],
   data() {
     return {
-      cantidad: 0, // Comienza con 0
-      precioUnitario: 190, // Precio de ejemplo
+      cantidad: 0,
+      precioUnitario: 190,
     };
   },
   computed: {
@@ -137,40 +112,10 @@ export default {
 </script>
 
 <style scoped>
-table {
-  width: 100%;
-  border-collapse: collapse;
-}
+/* Solo estilos específicos que no cubre Tailwind */
+@import url('https://fonts.googleapis.com/css2?family=Comfortaa&display=swap');
 
-th,
-td {
-  padding: 10px;
-  text-align: center;
-  border: 1px solid #ccc;
-}
-
-th {
-  background-color: #f2f2f2;
-}
-
-tr:nth-child(even) {
-  background-color: #f9f9f9;
-}
-
-.input-cantidad {
-  width: 60px;
-  text-align: center;
-  border: 2px solid #ccc;
-  padding: 5px;
-  font-size: 16px;
-  background-color: white;
-  /* Asegura que el fondo siempre sea visible */
-  box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.1);
-  /* Le da un ligero relieve */
-}
-
-h1 {
-  font-family: 'Mode G', serif;
-  font-size: 2.3rem;
+body {
+  font-family: 'Comfortaa', sans-serif;
 }
 </style>
